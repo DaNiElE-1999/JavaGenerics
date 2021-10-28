68ba777d51e62a7c6cbc0545b90dd6362bbf978a
Daniele Llazo <danielellazo@unyt.edu.al>
2021-10-28 20:35:32 +0200

    added exercise 3

---
+++ Exercises/src/main/java/Exercise3/Main.java
@@ -0,0 +1,12 @@
+package Exercise3;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        ArrayExchangeElements a = new ArrayExchangeElements();
+        a.test();
+
+    }//main
+
+}
