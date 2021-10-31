2c159d308fe2b9ebd4fd9560761e0c156e519344
Daniele Llazo <danielellazo@unyt.edu.al>
2021-10-31 13:06:19 +0100

    added exercise 9

---
+++ Exercises/src/main/java/Exercise9/Singleton.java
@@ -0,0 +1,15 @@
+package Exercise9;
+/*
+//Will the following class compile? If not, why?
+public class Singleton<T> {
+
+    public static T getInstance() {
+        if (instance == null)
+            instance = new Singleton<T>();
+
+        return instance;
+    }
+
+    private static T instance = null;
+}
+*/
